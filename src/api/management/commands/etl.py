cfb1b0088dc5cdf0e20920cdfa405f3f17dc2ce3
Nikita Efremov <nikefremov.00.008@hotmail.com>
2021-01-26 17:35:11 +0300

    Fix etl manage-cmd

--- src/api/management/commands/etl.py
+++ src/api/management/commands/etl.py
@@ -6,13 +6,12 @@ from django.core.management.base import BaseCommand
 from django.db.migrations.executor import MigrationExecutor
 from django.db import connections, DEFAULT_DB_ALIAS
 from django.forms.models import model_to_dict
+
 from progress.bar import Bar
 from requests.models import Request
-from api.models import (
-    Movies,
-    MovieActors,
-    MovieWriters
-)
+
+from api.models import Movie
+from api.logic.data_structures.enums import Positions
 
 def init_es() -> Request():
     url = f"{settings.BASE_ES_URL}movies"
@@ -133,27 +132,31 @@ def init_es() -> Request():
     return requests.request("PUT", url, headers=headers, data=payload)
 
 
+def movie_to_dict(movie : Movie) -> dict:
+    data = {}
+    data.update(model_to_dict(movie, exclude=["crew", "genre"]))
+    data["genre"] = ", ".join([genre.name for genre in movie.genre.all()])
+    data["actors_names"] = []
+    data["actors"] = []
+    for actor_person in movie.personposition_set.filter(position=Positions.ACTOR):
+        actor_name = actor_person.person_id.name
+        if actor_name != "N/A":
+            data["actors_names"].append(actor_name)
+            data["actors"].append({"id" : actor_person.person_id.id, "name" : actor_name})
+    data["writers_names"] = []
+    data["writers"] = []
+    writers = movie.personposition_set.filter(position=Positions.ACTOR)
+    for writer in writers:
+        name = writer.person_id.name
+        if name not in data["writers_names"] and name != "N/A":
+            data["writers_names"].append(name) 
+            data["writers"].append({"id" : writer.person_id.id, "name" : name})
+
 def extract_movies() -> dict:
     movies = []
-    bar = Bar('Processing', max=Movies.objects.count())
-    for movie in Movies.objects.all():
-        data = {}
-        data.update(model_to_dict(movie))
-        data["actors_names"] = []
-        data["actors"] = []
-        for actor in MovieActors.objects.filter(movie_id=movie):
-            actor_name = actor.actor.name
-            if actor_name != "N/A":
-                data["actors_names"].append(actor_name)
-                data["actors"].append({"id" : actor.actor.id, "name" : actor_name})
-        data["writers_names"] = []
-        data["writers"] = []
-        writers = MovieWriters.objects.filter(movie=movie)
-        for writer in writers:
-            name = writer.writer.name
-            if name not in data["writers_names"] and name != "N/A":
-                data["writers_names"].append(name) 
-                data["writers"].append({"id" : writer.writer.id, "name" : name})
+    bar = Bar('Processing', max=Movie.objects.count())
+    for movie in Movie.objects.all():
+        data = movie_to_dict(movie)
         movies.append(data)
         bar.next()
     bar.finish()
@@ -184,10 +187,10 @@ class Command(BaseCommand):
     help = 'Load data from SQLite database to PostgreSQL and to elasticsearch'
 
     def handle(self, *args, **options):
-        if init_es().status_code == 200:
-            if is_database_synchronized(DEFAULT_DB_ALIAS):
-                print('All migrations have been applied.')
-                load_movies_es(extract_movies())
-                print("All data is transfered to elasticsearch")
-            else:
-                print("Unapplied migrations found.")
+        init_es()
+        if is_database_synchronized(DEFAULT_DB_ALIAS):
+            print('All migrations have been applied.')
+            load_movies_es(extract_movies())
+            print("All data is transfered to elasticsearch")
+        else:
+            print("Unapplied migrations found.")
