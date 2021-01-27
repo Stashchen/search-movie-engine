67ea6a1a7e2732ade06809e6ef5455350ce3e3d4
Nikita Efremov <nikefremov.00.008@hotmail.com>
2021-01-27 19:07:31 +0300

    Updated requirements

--- src/api/management/commands/sqlite_to_postgres.py
+++ src/api/management/commands/sqlite_to_postgres.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 from progress.bar import Bar
 
 from django.core.management.base import BaseCommand
-
+from django.core.management import call_command
 from api.models import Movie, Person, PersonPosition, Genre
 from api.logic.data_structures.enums import Positions
 from api.management.commands._sqlite_models import (
@@ -18,7 +18,7 @@ def etl_person():
 
 def etl_movie():
     # Load all person (actors, directors and writers)
-    etl_person()
+    # etl_person()
 
     # Load all other data (movies, genres and person_position)
     bar = Bar("processing", max=Movies.select().count())
@@ -56,5 +56,7 @@ class Command(BaseCommand):
     help = 'Load data from SQLite database to PostgreSQL and to elasticsearch'
 
     def handle(self, *args, **options):
+        call_command("flush")
+        etl_person()
         etl_movie()
         print("All data are transfered from sqlite to postgresql")
